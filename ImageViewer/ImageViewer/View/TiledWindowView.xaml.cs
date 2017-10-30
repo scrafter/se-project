2f89619531a76a097f37d74c9e59bcf4cf7d19f1
Kamil Grzechnik <kamil.grzechnik17@gmail.com>
2017-10-30 19:59:45 +0100

    uniform grid

--- ImageViewer/ImageViewer/View/TiledWindowView.xaml.cs
+++ ImageViewer/ImageViewer/View/TiledWindowView.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -23,6 +24,11 @@ namespace ImageViewer.View
         public TiledWindowView()
         {
             InitializeComponent();
+        }
+
+        private void imageList_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var s = sender; 
         }
     }
 }
