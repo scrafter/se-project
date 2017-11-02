ac3f598c39532d289b5869de3ff1343de358dd5a
Kamil Grzechnik <kamil.grzechnik17@gmail.com>
2017-11-02 14:03:35 +0100

    Removing Item, New Window - double click

---
+++ ImageViewer/ImageViewer/ViewModel/ImageWindowViewModels/ImagePresenterViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageViewer.Model;
+using Prism.Events;
+using ImageViewer.Model.Event;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ImageViewer.ViewModel.ImageWindowViewModels
+{
+    public class ImagePresenterViewModel : BaseViewModel
+    {
+        private Image _displayedImage;
+        private ImageSource _imageSource;
+
+        public Image DisplayedImage
+        {
+            get => _displayedImage;
+            set
+            {
+                _displayedImage = value;
+                NotifyPropertyChanged();
+                if(_displayedImage != null)
+                    ImageSource = new BitmapImage(new Uri(_displayedImage.FilePath));
+            }
+        }
+
+        public ImageSource ImageSource
+        {
+            get => _imageSource;
+            set
+            { 
+                _imageSource = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public ImagePresenterViewModel()
+        {
+            _aggregator.GetEvent<DisplayImage>().Subscribe(item => { DisplayedImage = item; });
+        }
+    }
+}
