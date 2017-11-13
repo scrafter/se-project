87955d3a9555de2171c0c6e4565a1999efbeaf29
Tomasz Tarasiewicz <tarasiewicztomasz@gmail.com>
2017-11-13 21:23:11 +0100

    Shit doesn't work, losing my nerves

--- ImageViewer/ImageViewer/Methods/ImageSaver.cs
+++ ImageViewer/ImageViewer/Methods/ImageSaver.cs
@@ -12,38 +12,59 @@ public static class ImageSaver
 {
     public static void SafeToText(ObservableCollection<Image> list)
     {
-        string path = @"d:\ImagesKeeper.txt";
-        System.IO.File.WriteAllBytes(@"d:\ImagesKeeper.txt", new byte[0]);
-
-        StreamWriter sw = File.AppendText(path);
-        if (File.Exists(path))
+        string directory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\ImageViewer";
+        string filename = @"\ImagesKeeper.txt";
+        string path = directory + filename;
+        try
         {
+            System.IO.File.WriteAllBytes(path, new byte[0]);
+        }
+        catch(DirectoryNotFoundException e)
+        {
+            Directory.CreateDirectory(directory);
+            if (!File.Exists(path))
+                File.Create(path).Close();
+        }
+        finally
+        {
+            StreamWriter sw = File.AppendText(path);
             foreach (Image image in list)
             {
                 sw.WriteLine(image.Extension);
                 sw.WriteLine(image.FilePath);
                 sw.WriteLine(image.FileName);
             }
+            sw.Close();
         }
-        sw.Close();
     }
     public static void SendTheLoadedImages(ObservableCollection<Image> list)
     {
 
-        string path = @"d:\ImagesKeeper.txt";
-        var lineCount = File.ReadLines(path).Count();
-        var f = System.IO.File.ReadAllLines(path);
-
-        for (int i = 0; i < lineCount; i += 3)
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\ImageViewer\ImagesKeeper.txt";
+        try
         {
-            Image image = new Image
-            {
-                FileName = f[i + 2],
-                FilePath = f[i + 1],
-                Extension = (f[i])
-            };
-            App.Current.Dispatcher.Invoke(() => list.Add(image));
+            var lineCount = File.ReadLines(path).Count();
+            var f = System.IO.File.ReadAllLines(path);
 
+            for (int i = 0; i < lineCount; i += 3)
+            {
+                Image image = new Image
+                {
+                    FileName = f[i + 2],
+                    FilePath = f[i + 1],
+                    Extension = (f[i])
+                };
+                App.Current.Dispatcher.Invoke(() => list.Add(image));
+
+            }
+        }
+        catch(FileNotFoundException e)
+        {
+            return;
+        }
+        catch(DirectoryNotFoundException e)
+        {
+            return;
         }
     }
 }
