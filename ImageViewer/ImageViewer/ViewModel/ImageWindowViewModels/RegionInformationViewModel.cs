aaa1ac5d3232ad54e283acc89193c3a1147477bd
Tomasz Tarasiewicz <tarasiewicztomasz@gmail.com>
2017-11-26 20:44:08 +0100

    Variance and deviation values are now displayed. ToolTip display time extended to one hour.

--- ImageViewer/ImageViewer/ViewModel/ImageWindowViewModels/RegionInformationViewModel.cs
+++ ImageViewer/ImageViewer/ViewModel/ImageWindowViewModels/RegionInformationViewModel.cs
@@ -13,6 +13,33 @@ namespace ImageViewer.ViewModel.ImageWindowViewModels
         string _maxValues;
         string _averageValues;
         string _regionSize;
+        string _variances;
+        string _deviations;
+
+        public string Deviations
+        {
+            get
+            {
+                return _deviations;
+            }
+            set
+            {
+                _deviations = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public string Variances
+        {
+            get
+            {
+                return _variances;
+            }
+            set
+            {
+                _variances = value;
+                NotifyPropertyChanged();
+            }
+        }
 
         public string RegionSize
         {
@@ -73,6 +100,8 @@ namespace ImageViewer.ViewModel.ImageWindowViewModels
             try
             {
                 double[] averages = (double[])parameters["Averages"];
+                double[] variances = (double[])parameters["Variances"];
+                double[] deviations = (double[])parameters["Deviations"];
                 byte[] mins = (byte[])parameters["Mins"];
                 byte[] maxs = (byte[])parameters["Maxs"];
                 int regionWidth = (int)parameters["Width"];
@@ -81,6 +110,8 @@ namespace ImageViewer.ViewModel.ImageWindowViewModels
                 MinValues = $"R:  {mins[0]}\nG:  {mins[1]}\nB:  {mins[2]}\nA:  {mins[3]}";
                 MaxValues = $"R:  {maxs[0]}\nG:  {maxs[1]}\nB:  {maxs[2]}\nA:  {maxs[3]}";
                 RegionSize = $"Region size:\t{regionWidth} x {regionHeight}";
+                Variances = $"R:  {Math.Round(variances[0], 2)}\nG:  {Math.Round(variances[1], 2)}\nB:  {Math.Round(variances[2], 2)}\nA:  {Math.Round(variances[3], 2)}";
+                Deviations = $"R:  {Math.Round(deviations[0], 2)}\nG:  {Math.Round(deviations[1], 2)}\nB:  {Math.Round(deviations[2], 2)}\nA:  {Math.Round(deviations[3], 2)}";
             }
             catch (KeyNotFoundException)
             {
